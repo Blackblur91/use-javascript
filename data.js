3fcb42e6068507390d816444aae39d1152e297fc
Blackblur91 <feketebalazs04@gmail.com>
2024-01-04 09:27:37 +0100

    added some frontend

---
+++ data.js
@@ -0,0 +1,23 @@
+[
+  {
+  "id": 0,
+  "name": {
+    "first": "",
+    "middle": "",
+    "last": ""
+  },
+  "email": "",
+  "shipping": {
+    "country": "",
+    "zip": "",
+    "city": "",
+    "address": "",
+  },
+  "invoice": {
+    "country": "",
+    "zip": "",
+    "city": "",
+    "address": ""
+  }
+}
+]
